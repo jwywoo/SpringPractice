c36cdb10a9b9326ebea86f64308e409c411553e5
jwywoo <jwywoo26@gmail.com>
2023-09-04 21:43:57 +0900

    JWYWOO-Rest Template: 1st commit

---
+++ RestTemplate/sprint-resttemplate-server/src/test/java/com/example/sprintresttemplateserver/SprintResttemplateServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sprintresttemplateserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintResttemplateServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
