b5f23e0dc261135743588794acde582fec8cf399
jwywoo <jwywoo26@gmail.com>
2023-09-05 14:48:20 +0900

    JWYWOO-Rest Template: (Client) Sending a request from Client's service (getCallObject) and using RestTemplate Builder

--- RestTemplate/spring-resttemplate-client/src/main/java/com/example/springresttemplateclient/service/RestTemplateService.java
+++ RestTemplate/spring-resttemplate-client/src/main/java/com/example/springresttemplateclient/service/RestTemplateService.java
@@ -2,15 +2,39 @@ package com.example.springresttemplateclient.service;
 
 import com.example.springresttemplateclient.dto.ItemDto;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
+import java.net.URL;
 import java.util.List;
 
 @Slf4j
 @Service
 public class RestTemplateService {
+    private final RestTemplate restTemplate;
+
+    public RestTemplateService(RestTemplateBuilder builder) {
+        this.restTemplate = builder.build();
+    }
+
     public ItemDto getCallObject(String query) {
-        return null;
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:7070")
+                .path("/api/server/get-cal-obj")
+                .queryParam("query", query)
+                .encode()
+                .build()
+                .toUri();
+
+        log.info("uri = " + uri);
+        ResponseEntity<ItemDto> responseEntity = restTemplate.getForEntity(uri, ItemDto.class);
+        log.info("statusCode = " + responseEntity.getStatusCode());
+
+        return responseEntity.getBody();
     }
 
     public List<ItemDto> getCallList() {
