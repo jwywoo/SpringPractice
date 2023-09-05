58a42d0f3b4304aaddb73d925b1308e057fe87be
jwywoo <jwywoo26@gmail.com>
2023-09-05 13:34:04 +0900

    JWYWOO-Rest Template: (Client) Initial setting done

---
+++ RestTemplate/spring-resttemplate-client/src/main/java/com/example/springresttemplateclient/dto/ItemDto.java
@@ -0,0 +1,13 @@
+package com.example.springresttemplateclient.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class ItemDto {
+    private String title;
+    private int price;
+}
+
+
