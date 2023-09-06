a93cf6718819f1600ffab50753d0a70dae120380
jwywoo <jwywoo26@gmail.com>
2023-09-06 23:15:16 +0900

    JWYWOO: Entity Relationship: ManyToMany(Third table) Add timeStamp(JPA auditing)

--- jpa-advance/src/main/java/com/example/jpaadvance/JpaAdvanceApplication.java
+++ jpa-advance/src/main/java/com/example/jpaadvance/JpaAdvanceApplication.java
@@ -2,8 +2,10 @@ package com.example.jpaadvance;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class JpaAdvanceApplication {
 
     public static void main(String[] args) {
