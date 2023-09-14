904b5812ee4abd48624a84d60f49678ab22b176b
jwywoo <jwywoo26@gmail.com>
2023-09-14 09:22:46 +0900

    Used Products Selling API - Project setting done

---
+++ used-products-api/src/main/java/com/example/usedproductsapi/UsedProductsApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.usedproductsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsedProductsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsedProductsApiApplication.class, args);
+    }
+
+}
